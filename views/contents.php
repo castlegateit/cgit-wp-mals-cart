ef4d4aefb2aa20ba90a58a492cb725b43cd66c3b
John Hughes <john@castlegateit.co.uk>
2016-01-07 10:18:14 +0000

    Tweak contents message position

--- views/contents.php
+++ views/contents.php
@@ -2,16 +2,18 @@
 
 $cart = Cgit\MalsCart::getInstance();
 $contents = $cart->contents();
+$message = '';
 
 if ($contents) {
     $items = $contents['quantity'] > 1 ? 'items' : 'item';
     $price = $cart::formatCurrency($contents['total']);
-
-    echo '<p>' . $contents['quantity'] . ' ' . $items . ': ' . $price . '</p>';
+    $message = '<p>' . $contents['quantity'] . ' ' . $items . ': ' . $price
+        . '</p>';
 }
 
 ?>
 <div class="mals-cart-view">
+    <?= $message ?>
     <form action="<?= $cart->urls['view'] ?>" method="post">
 
         <input type="hidden" name="userid" value="<?= CGIT_MALS_CART_ID ?>" />
